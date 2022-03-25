a004adf08132b9bc871b928b08ab3463f334a7f4
EricWeng23 <ericw39@nycstudents.net>
2022-03-25 13:57:42 -0400

    edits

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <style>
-  @import './assets/style.css';
+  @import './assets/main.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
