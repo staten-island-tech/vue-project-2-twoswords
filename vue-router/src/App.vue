b4d166ee86af91125808e453a36d57e342af9ef9
EricWeng23 <90655476+EricWeng23@users.noreply.github.com>
2022-03-25 23:06:36 -0400

    Revert "small edits"
    This reverts commit 76fede924e0aaeb7a77ad18348255c2f48920451.

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -6,16 +6,7 @@
   <router-view/>
 </template>
 
-<script>
-import Button from '../components/Button.vue'
-import NavBar from '../components/NavBar.vue'
-export default {
-  
-}
-</script>
-
 <style>
-  @import './assets/style.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
