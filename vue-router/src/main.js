05c41c0bfff8f5f6711ec4b5ac7b48c760e5bf74
EricWeng23 <90655476+EricWeng23@users.noreply.github.com>
2022-03-26 00:34:55 -0400

    commit

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from '../store/index'
 
 //global styles
 import './assets/style.css'
 
 //import store
-import store from './store/index'
 
 createApp(App).use(router).use(store).mount('#app')
