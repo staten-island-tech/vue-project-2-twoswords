fbd15420c5022b725ab52644025e7949475d3c8b
EricWeng23 <90655476+EricWeng23@users.noreply.github.com>
2022-03-27 20:59:27 -0400

    edits

---
+++ vue-router/src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card">
+        <h2>{{name}}</h2>
+        <img :src="image" alt="">
+        <h2>${{description}}</h2>
+        <span><slot></slot></span>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: "Card",
+    props: {
+        name: String,
+        image: String,
+        price: Number,
+    },
+}
+</script>
+
+<style scoped>
+.card{
+    margin-top: 2rem;
+    width: 30rem;
+    height: 30rem;
+    border: 0.0625rem solid lightgray;
+    border-radius: 1rem;
+    box-shadow: 1rem 1rem 1rem rgba(0, 0, 0, 0.555);
+    transition-duration: all ease 2s;
+}
+img {
+  width: 30%;
+  height: 50%;
+}
+.product-card:hover {
+  transform: scale(1.05);
+}
+</style>
