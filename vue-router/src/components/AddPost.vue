adf30c28d04c1ee7eb96e520f9bc106c7c5c69a4
HaoChenSupreme <HAoranc3@nycstudents.net>
2022-04-27 13:57:48 -0400

    rtrrrer

--- vue-router/src/components/AddPost.vue
+++ vue-router/src/components/AddPost.vue
@@ -1,85 +1,21 @@
 <template>
-    <div>
-      <form class="create-post" v-on:submit="createPost">
+    <div id="add-post">
+        <h2>Create Post</h2>
+        <form v-if="!submitted">
+            <label>Post Title:</label>
+            <input type="text" v-model.lazy="post.title" required />
+            <label>Blog Content:</label>
+            <textarea v-model.lazy="blog.content"></textarea>
+            <label>Author:</label>
+            <input type="text" v-model.lazy="post.title" required />
+
+            </div>
+
+        </form>
       
-      <div class="form-control">
-        <label for="title"> Post Title</label>
-        <input type="text" v-model="newPost.title" id="title" placeholder="your post title" />
-      </div>
-
-      <div class="form-control">
-        <label for="cover"> Post Cover</label>
-        <input type="file" id="cover" v-on:change="handleFile"  />
-      </div> 
-      
-      <input type="submit" :disabled='isDisabled' value="Create Post" />
-
-      <div class="form-control">
-        <label for="content"> Post Content</label>
-        <textarea v-model="newPost.content" id="content" placeholder="your post content"/>
-      </div>
-
-     
-      </form>
-
-      <p class="progress">Uploading file: {{fileProgress}} %</p>
-
-
     </div>
 </template>
 
 <script>
-import firebase from "../firebase/config";
-    export default {
-       name: "AddPost",
-       data(){
-           return {
-               newPost: {
-                   title: "",
-                   content: "",
-                   cover: ""
-               },
-               isDisabled: false,
-               fileProgress: ""
-           }
-       },
-       methods: {
-           async createPost(e){
-               this.isDisabled = true;
-               e.preventDefault();
-             
-              let d = "";
-              const storageRef = firebase.storage.ref();
-              const storageChild = storageRef.child(this.newPost.cover.name);
-              const postCover = storageChild.put(this.newPost.cover);
-              await new Promise(resolve => {
-                   postCover.on("state_changed", (snapshot) => {
-                        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                        this.fileProgress = progress;
-                    },(error) => {
-                        //error
-                        console.log(error);
-                    },  async() => {
-                        //completed
-                        const downloadURL = await storageChild.getDownloadURL();
-                        d = downloadURL;
-                        console.log(d);
-                        resolve();
-                    });
-              });
-                let _newPost = {
-                    title: this.newPost.title,
-                    content: this.newPost.content,
-                } 
-                firebase.createPost(d, _newPost)
-                .then(() => {
-                    this.$router.push({name: "home"});
-                }).catch(err => console.log(err));
-           
-           },
-           handleFile(e){
-               this.newPost.cover = e.target.files[0];
-           }
-       }
-    }
+
 </script>
