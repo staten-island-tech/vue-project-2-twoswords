95e16d1546f427096a5cf0ab321319ba5c5da4d6
HaoChenSupreme <HAoranc3@nycstudents.net>
2022-04-28 13:57:59 -0400

    rgrgrg

--- vue-router/src/components/AddPost.vue
+++ vue-router/src/components/AddPost.vue
@@ -6,12 +6,30 @@
             <input type="text" v-model.lazy="post.title" required />
             <label>Blog Content:</label>
             <textarea v-model.lazy="blog.content"></textarea>
-            <label>Author:</label>
-            <input type="text" v-model.lazy="post.title" required />
-
+            <div id="checkboxes">
+                <label>Food</label>
+                <input type="checkbox" value="food" v-model="post.categries"/>
+                <label>Travel</label>
+                <input type="checkbox" value="food" v-model="post.categries"/>
+                <label>Story</label>
+                <input type="checkbox" value="food" v-model="post.categries"/>
+                <label>Recommendation</label>
+                <input type="checkbox" value="food" v-model="post.categries"/>
+                <label>Food</label>
+                <input type="checkbox" value="food" v-model="post.categries"/>
             </div>
-
+            <label>Author:</label>
+            <input type="text" v-model.lazy="post.author" required />
         </form>
+        <div v-if="submitted">
+            <h3>Muy Xapowo</h3>
+        </div>
+        <div id="preview">
+            <h3>Preview Post</h3>
+            <p>Post title: {{post.title}}</p>
+            <p>Post content:</p>
+            <p>{{post.content}}</p>
+        </div>
       
     </div>
 </template>
