691f8cb452c4e1a799dc219896ada4c3c97fbf63
HaoChenSupreme <HAoranc3@nycstudents.net>
2022-04-29 13:58:07 -0400

    wef

--- vue-router/src/components/AddPost.vue
+++ vue-router/src/components/AddPost.vue
@@ -5,18 +5,18 @@
             <label>Post Title:</label>
             <input type="text" v-model.lazy="post.title" required />
             <label>Blog Content:</label>
-            <textarea v-model.lazy="blog.content"></textarea>
+            <textarea v-model.lazy="post.content"></textarea>
             <div id="checkboxes">
                 <label>Food</label>
-                <input type="checkbox" value="food" v-model="post.categries"/>
+                <input type="checkbox" value="food" v-model="post.categories"/>
                 <label>Travel</label>
-                <input type="checkbox" value="food" v-model="post.categries"/>
+                <input type="checkbox" value="food" v-model="post.categories"/>
                 <label>Story</label>
-                <input type="checkbox" value="food" v-model="post.categries"/>
+                <input type="checkbox" value="food" v-model="post.categories"/>
                 <label>Recommendation</label>
-                <input type="checkbox" value="food" v-model="post.categries"/>
+                <input type="checkbox" value="food" v-model="post.categories"/>
                 <label>Food</label>
-                <input type="checkbox" value="food" v-model="post.categries"/>
+                <input type="checkbox" value="food" v-model="post.categories"/>
             </div>
             <label>Author:</label>
             <input type="text" v-model.lazy="post.author" required />
@@ -29,11 +29,37 @@
             <p>Post title: {{post.title}}</p>
             <p>Post content:</p>
             <p>{{post.content}}</p>
+            <p>Post Categories:</p>
+            <ul>
+                <li v-for="category in post.categories">{{ category }}</li>
+            </ul>
+            <p>Author: {{ post.author }}</p>
         </div>
-      
     </div>
 </template>
 
 <script>
+export default{
+    data() {
+        return{
+            post:{
+                title:"",
+                content:"",
+                categories:[],
+                author:""
+            },
+            submitted: false
+        }
+    },
+    methods: {
+        post: function(){
+            this.$http.post('https://vue-firebase-1bc25-default-rtdb.firebaseio.com/posts.json',{
+
+            }
+
+            )
+        }
+    }
+}
 
 </script>
