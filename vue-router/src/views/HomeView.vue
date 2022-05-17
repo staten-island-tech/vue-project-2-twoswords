5ccdce16d2fc85689396a6cde79ecb49d17e5b37
HaoChenSupreme <HAoranc3@nycstudents.net>
2022-05-17 13:56:35 -0400

    dgdfgdfb

--- vue-router/src/views/HomeView.vue
+++ vue-router/src/views/HomeView.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="home">
+  <div id="showposts">
+    <h1>All Posts</h1>
+    <input type="text" v-model="search" placeholder="search blogs">
+    <div v-for="post in filteredposts" class="singleblog">
+      <router-link v-bind:to="'/post/' + post.id"><h2>{{post.title | to-uppercase}}</h2></router-link>
+      <article>{{post.content | snippet}}</article>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'HomeView',
-  components: {
+  export default{
+    data(){
+      return{
+        blog:[],
+        search:''
+      }
+    }
   }
 }
 </script>
