909d30414829e9b2ffc428c8804d593270a1d6e9
HaoChenSupreme <HAoranc3@nycstudents.net>
2022-05-26 13:46:04 -0400

    656

--- vue-router/src/components/Card.vue
+++ vue-router/src/components/Card.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="card">
+    <div class="container">
+      <div class="cardlist">
         <h2 class="cardname">{{name}}</h2>
-        <img class="cardimg" :src="image" alt="">
+        <div class="cardinfo">
+          <img class="cardimg" :src="image" alt=""> 
+          <p class="cardcontent">{{country.substring(0,3000)}}</p>
+        </div>
         <span><slot></slot></span>
-        <div class="back">
-          <h2 class="cardcontent">{{country}}</h2>
-          </div>
+      </div>
+      
     </div>
-    
-
 </template>
 
 <script>
@@ -26,37 +27,53 @@ export default {
 </script>
 
 <style scoped>
-.card{
-    display: flex;
-    flex-direction: column;
+.cardlist{
+     display: flex;
+   flex-direction: column;
     justify-content: center;
     border: 0.5rem solid;
-  border-color: var(--primary);
-  border-radius: 2rem;
-  font-weight: bold;
+    border-color: var(--primary);
+    border-radius: 2rem;
+    font-weight: bold;
     flex-wrap: wrap;
     align-items: center;
-    background-color: white ;
+    background-color: rgb(227, 243, 255) ;
     margin-top: 5rem;
-    width: 40rem;
-    height: 40rem;
+    width: 100rem;
+    height: 49rem;
     border: 0.0625rem solid lightgray;
     border-radius: 1rem;
     box-shadow: 1rem 1rem 1rem rgba(0, 0, 0, 0.555);
-    transition-duration: all ease 2s;
     margin: 0.5rem auto;
-  align-items: center;
-  position: relative;
-  perspective: 62.5rem;
-  transform-style: preserve-3d;
-  transform: all 3s ease;
-  backface-visibility: hidden;
-  -webkit-backface-visibility: hidden;
+    margin-top: 4rem;
+    align-items: center;
+    position: relative;
+}
+.container{
+ display: flex;
+ flex-wrap: wrap;
+ justify-content: space-evenly;
 }
 img {
-  width: 50%;
-  height: 70%;
-  object-fit: cover;
+  width: 20rem;
+  border-radius: 1rem;
+}
+.cardinfo{
+  display: flex;
+ justify-content: center;
+ margin-top: 1rem;
+ margin-bottom: 1rem;
+ width: 90rem;
+ height: 35rem;
+ padding: 2.5rem;
+ border: 0.0625rem solid rgb(166, 166, 166);
+ border-radius: 1rem;
+ background-color: rgb(252, 228, 228);
+}
+.cardname{
+   margin: 0;
+  font-size: 2rem;
+  justify-content: center;
 }
 .back {
   padding: 1rem;
@@ -75,13 +92,22 @@ img {
   transform: rotateY(180deg);
 }
 
-.card:focus {
+.card:hover {
   transform:  rotateY(180deg);
   transition: all 3s ease;
 }
 
 .card:focus .back {
   transform: rotateY(180deg);
+}
+
+.cardcontent{
+  font-size: 1.5rem;
+  width: 70rem;
+  margin-left: 5rem;
+  border-radius: 1rem;
+  background-color: rgb(227, 227, 227);
+  padding: 1.5rem;
 }
 
 </style>
