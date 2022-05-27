0573828b8c9fa7107a606c1402a9c650592939cd
HaoChenSupreme <90791814+HaoChenSupreme@users.noreply.github.com>
2022-05-27 10:42:47 -0400

    dawdw

--- vue-router/src/views/AddCity.vue
+++ vue-router/src/views/AddCity.vue
@@ -12,8 +12,8 @@
               <input type="text" class="form-control" id="addimg" placeholder="Kid friendly pls..." v-model="image"/>
             </div>
             <div class="form-group">
-              <label for="text">Content</label>
-              <textarea type="text" class="form-control" id="text" placeholder="Type something here..." v-model="country"></textarea>
+              <label for="text">Content (Max Word Length = 1200)</label>
+              <textarea type="text" maxlength="1200" class="form-control" id="text" placeholder="Type something here..." v-model="country"></textarea>
             </div>
             <button class="addbtn">Create Post</button>
           </form>
